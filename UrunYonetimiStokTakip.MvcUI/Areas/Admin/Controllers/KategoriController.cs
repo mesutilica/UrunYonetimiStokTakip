0989d4848e44643481219670a125c29e6eed9d91
mesutilica <mesut.ilica@gmail.com>
2021-09-10 23:30:50 +0300

    Mvc Kategori ve kullanıcı yönetim ekranları yapıldı

--- UrunYonetimiStokTakip.MvcUI/Areas/Admin/Controllers/KategoriController.cs
+++ UrunYonetimiStokTakip.MvcUI/Areas/Admin/Controllers/KategoriController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using BL;
 using Entities;
@@ -15,6 +13,83 @@ namespace UrunYonetimiStokTakip.MvcUI.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View(manager.GetAll());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    kategori.EklenmeTarihi = DateTime.Now;
+                    var sonuc = manager.Add(kategori);
+                    if (sonuc > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else ModelState.AddModelError("", "Kayıt Eklenemedi!");
+                }
+                catch (Exception hata) //buradaki hata nesnesinden hata detaylarına ulaşabiliriz
+                {
+                    ModelState.AddModelError("", "Hata Oluştu! Kayıt Eklenemedi!");
+                }
+            }
+            return View();
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = manager.Get(id.Value);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+        [HttpPost]
+        public ActionResult Edit(Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                manager.Update(kategori);
+
+                return RedirectToAction("Index");
+            }
+            return View(kategori);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = manager.Get(id.Value);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            try
+            {
+                Kategori kategori = manager.Get(id.Value);
+                manager.Delete(kategori.Id);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("","Hata oluştu!");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ UrunYonetimiStokTakip.MvcUI/Areas/Admin/Controllers/KullaniciController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using BL;
+using Entities;
+
+namespace UrunYonetimiStokTakip.MvcUI.Areas.Admin.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        KullaniciManager manager = new KullaniciManager();
+        // GET: Admin/Kullanici
+        public ActionResult Index()
+        {
+            return View(manager.GetAll());
+        }
+                
+        // GET: Admin/Kullanici/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Kullanici/Create
+        [HttpPost]
+        public ActionResult Create(Kullanici kullanici)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var sonuc = manager.Add(kullanici);
+                    if (sonuc > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else ModelState.AddModelError("", "Kayıt Eklenemedi!");
+                }                
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu! Kayıt Eklenemedi!");
+            }
+            return View();
+        }
+
+        // GET: Admin/Kullanici/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = manager.Get(id.Value);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanici);
+        }
+
+        // POST: Admin/Kullanici/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Kullanici kullanici)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    manager.Update(kullanici);
+
+                    return RedirectToAction("Index");
+                }
+                return View(kullanici);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Kullanici/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = manager.Get(id.Value);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanici);
+        }
+
+        // POST: Admin/Kullanici/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                manager.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
